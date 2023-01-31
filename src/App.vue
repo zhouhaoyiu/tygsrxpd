db27f2b68c3f922d856a5441f875a91d858077bf
Mystic <67226385+zhouhaoyiu@users.noreply.github.com>
2023-01-31 16:35:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+interface IWorkForm {
+  workIdentifier: string;
+}
+
+type TWorkText = string;
+let workText: Ref<TWorkText> = ref("");
+let workForm: Ref<IWorkForm> = ref({
+  workIdentifier: "",
+});
+</script>
+
+<template>
+  <div id="app">
+    <div>
+      文字识别
+      <el-input
+        type="textarea"
+        v-model="workText"
+        placeholder="请输入工单编号"
+      ></el-input>
+      <el-button type="primary">提交</el-button>
+    </div>
+    <div>
+      <div>新建工单</div>
+      <el-input
+        v-model="workForm.workIdentifier"
+        placeholder="请输入工单编号"
+      ></el-input>
+      <el-button type="primary">提交</el-button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
