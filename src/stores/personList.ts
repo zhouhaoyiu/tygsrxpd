c79c9c9bd984a915ba37a906a843430f2857a083
Radwimps <67226385+zhouhaoyiu@users.noreply.github.com>
2025-09-24 19:41:45 +0800

    fix

--- src/stores/personList.ts
+++ src/stores/personList.ts
@@ -2,21 +2,23 @@ import { ref } from "vue";
 import { defineStore } from "pinia";
 
 // 定义人员列表
-const usePersonList = defineStore({
-  id: "personList",
-  state: () => ({
-    personList: ref([]) as unknown as Record<string, any>[],
-  }),
-  getters: {
-    getPersonList: (state) => {
-      return state.personList;
+const usePersonList = defineStore(
+  "personList",
+  {
+    state: () => ({
+      personList: ref([]) as unknown as Record<string, any>[],
+    }),
+    getters: {
+      getPersonList: (state) => {
+        return state.personList;
+      },
     },
-  },
-  actions: {
-    setPersonList(personList: Record<string, any>[]) {
-      this.personList = personList;
+    actions: {
+      setPersonList(personList: Record<string, any>[]) {
+        this.personList = personList;
+      },
     },
-  },
-});
+  }
+);
 
 export default usePersonList;
